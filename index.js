0cf40bba1330b118f491025390fcd3b925bfa7a0
andreysulymka <andreysulymka5@gmail.com>
2023-05-19 18:59:45 +0300

    add all scripts

---
+++ index.js
@@ -0,0 +1,38 @@
+const contacts = require("./contacts")
+const {program} = require("commander")
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+          const allContacts = await contacts.listContacts();
+          return console.table(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id)
+      return console.table(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact({name, email, phone})
+      return console.table(newContact);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id)
+      return console.log("Contact has been deleted");
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+program
+.option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
+  
+program.parse();
+
+const options = program.opts();
+
+invokeAction(options)
